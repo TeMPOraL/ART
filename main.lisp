41d1fc03951c8c519da408e7c55c8bf1af97b955
Jacek Zlydach <temporal.pl@gmail.com>
2011-09-22 20:56:19 +0100

    Fixed idents.

--- main.lisp
+++ main.lisp
@@ -11,10 +11,10 @@
 
 (defpackage :trc.art
   (:use :common-lisp
-		:hunchentoot
-		:ht-simple-ajax
-		:cl-who
-		:parenscript))
+                :hunchentoot
+                :ht-simple-ajax
+                :cl-who
+                :parenscript))
 
 (in-package :trc.art)
 
@@ -33,20 +33,20 @@
 ;;; other tools
 (defun tag-list-with-indices (list)
   (loop for i from 0 to (length list)
-	    for elem in list
-	   collecting (cons i elem)))
+            for elem in list
+           collecting (cons i elem)))
 
 ;;; lower-level functions
 (defun init-ART ()
   (setf *clusters* 1
-		*top-down-weights* (make-array 1
-									   :initial-element (make-array +input-size+)
-									   :fill-pointer 1
-									   :adjustable t)
-		*bottom-up-weights* (make-array 1
-										:initial-element (make-array +input-size+)
-										:fill-pointer 1
-										:adjustable t)))
+        *top-down-weights* (make-array 1
+                                       :initial-element (make-array +input-size+)
+                                       :fill-pointer 1
+                                       :adjustable t)
+        *bottom-up-weights* (make-array 1
+                                        :initial-element (make-array +input-size+)
+                                        :fill-pointer 1
+                                        :adjustable t)))
 
 (defun save-ART-match (match)
   ;; FIXME FIXME don't use.
@@ -64,29 +64,29 @@
 (defun run-art-on-input (input)
   ;; ART magic algorithm
   (let* ((input-sum (reduce #'+ input))
-		;; top-layer-reaction aka. f2.
-		 (top-layer-reaction (map 'vector (lambda (weight-set)
-											(dot-product weight-set input))
-								  *bottom-up-weights*0))
-		 ;; Sort the neurons by top-layer-reaction (descending),
-		 ;; attaching an indice to each value so that we can also have
-		 ;; a corresponding information about which neuron is which.
-		 (neurons (sort (tag-list-with-indices (coerce top-layer-reaction 'list))
-						#'>
-						:key #'cdr)))
-
-	;; Now that we have the above, we need to iterate over `neurons',
-	;; checking each one if it crosses the activation threshold; if it
-	;; does, we update weights; if it doesn't, we add a new one (and
-	;; set up weights).
-	(loop for neuron in neurons
-		 for activation-sum = (dot-product input (elt *top-down-weights* (car neuron)))
-		 ;; foo
-
-		 ;; if (not (should-reset-p activation-sum input-sum)) break
-	     ;; loop. I need to detect if we run out of entries in the
-	     ;; list - if so, we need to add a new cluster.
-		 )
+         ;; top-layer-reaction aka. f2.
+         (top-layer-reaction (map 'vector (lambda (weight-set)
+                                            (dot-product weight-set input))
+                                  *bottom-up-weights*0))
+         ;; Sort the neurons by top-layer-reaction (descending),
+         ;; attaching an indice to each value so that we can also have
+         ;; a corresponding information about which neuron is which.
+         (neurons (sort (tag-list-with-indices (coerce top-layer-reaction 'list))
+                        #'>
+                        :key #'cdr)))
+    
+    ;; Now that we have the above, we need to iterate over `neurons',
+    ;; checking each one if it crosses the activation threshold; if it
+    ;; does, we update weights; if it doesn't, we add a new one (and
+    ;; set up weights).
+    (loop for neuron in neurons
+       for activation-sum = (dot-product input (elt *top-down-weights* (car neuron)))
+       ;; foo
+         
+       ;; if (not (should-reset-p activation-sum input-sum)) break
+       ;; loop. I need to detect if we run out of entries in the
+       ;; list - if so, we need to add a new cluster.
+         )
 
 
 )
@@ -112,9 +112,9 @@
 
 (define-easy-handler (art :uri "/art") ()
   (with-html-output-to-string (*standard-output* nil :prologue t)
-	(:html :xmlns "http://www.w3.org/1999/xhtml"
-		   (:head (:title "ART"))
+        (:html :xmlns "http://www.w3.org/1999/xhtml"
+                   (:head (:title "ART"))
 
-		   (:body
-			(:h1 "ART networkz ftw!")
-			(:div "Some dummy text.")))))
+                   (:body
+                        (:h1 "ART networkz ftw!")
+                        (:div "Some dummy text.")))))
