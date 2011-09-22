86d7dd132f90d6bad62100b0f0664c314fead97e
Jacek Zlydach <jacek.zlydach@stfc.ac.uk>
2011-09-22 16:26:03 +0100

    More of the code. ART algorithm is being fleshed out.'

--- main.lisp
+++ main.lisp
@@ -26,6 +26,16 @@
 
 (defvar *clusters* 0 "Clusters found so far.")
 
+;;; math tools
+(defun dot-product (v1 v2)
+  (reduce #'+ (map 'vector (lambda (x y) (* x y)) v1 v2)))
+
+;;; other tools
+(defun tag-list-with-indices (list)
+  (loop for i from 0 to (length list)
+	    for elem in list
+	   collecting (cons i elem)))
+
 ;;; lower-level functions
 (defun init-ART ()
   (setf *clusters* 1
@@ -38,12 +48,55 @@
 										:fill-pointer 1
 										:adjustable t)))
 
+(defun save-ART-match (match)
+  ;; FIXME FIXME don't use.
+  (incf *clusters*)
+  (vector-push-extend *top-down-weights* match)
+  (vector-push-extend *bottom-up-weights* match)) ;; FIXME copy? and find out how/what by to extend?
+
+
 ;;; high-level interface functions
-(defun reset-ART ())
+(defun reset-ART ()
+  (init-ART))
 
 (defun train-ART (training-set-id))
 
-(defun run-art-on-input (input))
+(defun run-art-on-input (input)
+  ;; ART magic algorithm
+  (let* ((input-sum (reduce #'+ input))
+		;; top-layer-reaction aka. f2.
+		 (top-layer-reaction (map 'vector (lambda (weight-set)
+											(dot-product weight-set input))
+								  *bottom-up-weights*0))
+		 ;; Sort the neurons by top-layer-reaction (descending),
+		 ;; attaching an indice to each value so that we can also have
+		 ;; a corresponding information about which neuron is which.
+		 (neurons (sort (tag-list-with-indices (coerce top-layer-reaction 'list))
+						#'>
+						:key #'cdr)))
+
+	;; Now that we have the above, we need to iterate over `neurons',
+	;; checking each one if it crosses the activation threshold; if it
+	;; does, we update weights; if it doesn't, we add a new one (and
+	;; set up weights).
+	(loop for neuron in neurons
+		 for activation-sum = (dot-product input (elt *top-down-weights* (car neuron)))
+		 ;; foo
+
+		 ;; if (not (should-reset-p activation-sum input-sum)) break
+	     ;; loop. I need to detect if we run out of entries in the
+	     ;; list - if so, we need to add a new cluster.
+		 )
+
+
+)
+  
+  ;; preparations
+
+  ;; ART magic concurrency stuff
+
+  ;; 
+  )
 
 
 ;;; HTTP management code
